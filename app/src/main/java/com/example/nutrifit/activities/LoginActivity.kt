6d33b3627a4ef65166670a72030695effecb58e7
jonywrrior2 <cristianbersabe@gmail.com>
2024-03-24 14:24:45 +0100

    creando login

--- app/src/main/java/com/example/nutrifit/activities/LoginActivity.kt
+++ app/src/main/java/com/example/nutrifit/activities/LoginActivity.kt
@@ -1,4 +1,13 @@
 package com.example.nutrifit.activities
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nutrifit.R
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
 }
