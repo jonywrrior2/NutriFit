b0840dbc153fc3663ceae59647f24723b258089b
jonywrrior2 <cristianbersabe@gmail.com>
2024-03-24 15:19:31 +0100

    Diseño Login acabado y splashScreen

--- app/src/main/java/com/example/nutrifit/activities/InicioActivity.kt
+++ app/src/main/java/com/example/nutrifit/activities/InicioActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nutrifit.R
 
@@ -15,9 +17,12 @@ class InicioActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
 
+        val textViewNutriFit = findViewById<TextView>(R.id.textViewNutriFit)
+        val scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_up_down)
+        textViewNutriFit.startAnimation(scaleAnimation)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@InicioActivity, MainActivity::class.java)
+            val intent = Intent(this@InicioActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, SPLASH_TIME_OUT)
