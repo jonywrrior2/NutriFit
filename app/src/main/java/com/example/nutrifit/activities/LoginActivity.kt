9758c54a7bdb017a6dce77b5868300d6d9cdaaaa
jonywrrior2 <cristianbersabe@gmail.com>
2024-03-27 15:56:33 +0100

    Login diseño acabado

--- app/src/main/java/com/example/nutrifit/activities/LoginActivity.kt
+++ app/src/main/java/com/example/nutrifit/activities/LoginActivity.kt
@@ -1,13 +1,20 @@
 package com.example.nutrifit.activities
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.example.nutrifit.R
+import com.example.nutrifit.databinding.ActivityLoginBinding
 
 class LoginActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
