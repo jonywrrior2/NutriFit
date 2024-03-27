6122c926fbf8da437b2419cac119f9d76f5c290e
jonywrrior2 <cristianbersabe@gmail.com>
2024-03-27 14:58:28 +0100

    Terminado layouts signUp y login (a falta de poner la posibilidad de ver la contraseña)

--- app/src/main/java/com/example/nutrifit/activities/SignUpDBActivity.kt
+++ app/src/main/java/com/example/nutrifit/activities/SignUpDBActivity.kt
@@ -1,10 +1,14 @@
 package com.example.nutrifit.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Spinner
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nutrifit.R
 import com.example.nutrifit.databinding.ActivitySignupdbBinding
@@ -13,42 +17,44 @@ class SignUpDBActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupdbBinding
     private lateinit var factorActividadSpinner: Spinner
     private lateinit var objetivoSpinner: Spinner
+    private lateinit var alturaTxt: EditText
+    private lateinit var pesoTxt: EditText
 
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupdbBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        // Obtener referencia al Spinner
+        // Obtener referencia al Spinner de Factor de Actividad
         factorActividadSpinner = findViewById(R.id.factorActividadSpinner)
+        alturaTxt = findViewById(R.id.alturaTxt)
+        pesoTxt = findViewById(R.id.pesoTxt)
 
-        // Crear un ArrayAdapter usando el array de strings definido en strings.xml
-        val adapter = ArrayAdapter.createFromResource(
+        // Crear un ArrayAdapter usando el array de strings definido en strings.xml para Factor de Actividad
+        val factorActividadAdapter = ArrayAdapter.createFromResource(
             this,
             R.array.factor_actividad_values,
-            R.layout.spinner_item_layout// layout spinner
+            R.layout.spinner_item_layout
         )
+        factorActividadAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        factorActividadSpinner.adapter = factorActividadAdapter
 
-        // Especificar el layout a usar cuando la lista de opciones aparezca
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        // Aplicar el adaptador al Spinner
-        factorActividadSpinner.adapter = adapter
-
-        // Manejar la selección del usuario
+        // Manejar la selección del Spinner de Factor de Actividad
         factorActividadSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
                 val selectedFactor = parent.getItemAtPosition(position).toString()
-                // TODO Aquí puedes hacer algo con el factor de actividad seleccionado
+                // Aquí puedes hacer algo con el factor de actividad seleccionado
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
-                //TODO Manejar la situación en la que no se selecciona nada
+                // Manejar la situación en la que no se selecciona nada
             }
-
         }
+
+        // Obtener referencia al Spinner de Objetivo
         objetivoSpinner = findViewById(R.id.objetivoSpinner)
 
         // Crear un ArrayAdapter usando el array de strings definido en strings.xml para Objetivo
@@ -72,5 +78,38 @@ class SignUpDBActivity : AppCompatActivity() {
             }
         }
 
+        // Agregar OnTouchListener al EditText de altura
+        alturaTxt.setOnTouchListener { view, event ->
+            // Verificar si se hizo clic en el icono de información
+            if (event.action == MotionEvent.ACTION_UP && event.rawX >= (alturaTxt.right - alturaTxt.compoundDrawables[2].bounds.width())) {
+                // Si el clic se encuentra dentro del área del icono de información
+                showDialog("Altura (en cm)", "Introduce tu altura en centímetros.")
+                return@setOnTouchListener true
+            }
+            return@setOnTouchListener false
+        }
+
+        pesoTxt.setOnTouchListener { view, event ->
+            // Verificar si se hizo clic en el icono de información
+            if (event.action == MotionEvent.ACTION_UP && event.rawX >= (pesoTxt.right - pesoTxt.compoundDrawables[2].bounds.width())) {
+                // Si el clic se encuentra dentro del área del icono de información
+                showDialog("Peso (en kg)", "Introduce tu peso en kilogramos.")
+                return@setOnTouchListener true
+            }
+            return@setOnTouchListener false
+        }
+    }
+
+    // Función para mostrar el diálogo
+    private fun showDialog(title: String, message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 }
+
--- app/src/main/java/com/example/nutrifit/activities/SignUpDPActivity.kt
+++ app/src/main/java/com/example/nutrifit/activities/SignUpDPActivity.kt
@@ -1,13 +1,46 @@
 package com.example.nutrifit.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nutrifit.R
+import com.example.nutrifit.databinding.ActivitySignupdpBinding
+
 
 class SignUpDPActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupdpBinding
+    private lateinit var sexoSpinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signupdp)
+        binding = ActivitySignupdpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        sexoSpinner = findViewById(R.id.sexoSpinner)
+
+        val factorActividadAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.array_sexo,
+            R.layout.spinner_item_layout
+        )
+        factorActividadAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        sexoSpinner.adapter = factorActividadAdapter
+
+        // Manejar la selección del Spinner de Factor de Actividad
+        sexoSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedFactor = parent.getItemAtPosition(position).toString()
+                // Aquí puedes hacer algo con el factor de actividad seleccionado
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Manejar la situación en la que no se selecciona nada
+            }
+        }
 
     }
 }
