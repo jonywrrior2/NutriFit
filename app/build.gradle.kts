7ba18155f6078121c625d2503b2b308033d79e43
jonywrrior2 <cristianbersabe@gmail.com>
2024-03-28 12:11:46 +0100

    Implementación FireBase al proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,13 +43,19 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    //Material Design
     implementation("com.google.android.material:material:1.11.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.core:core-splashscreen:1.0.1")
+
+
+
+    // Implementacion de firebase
+    implementation("com.google.firebase:firebase-firestore:24.11.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
 }
