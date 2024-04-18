1a860c4cfe5d4dda1dd5157ee514c0e811511d83
jonywrrior2 <cristianbersabe@gmail.com>
2024-04-18 16:25:19 +0200

    Empezando layout de main añadir comida y evento de volver de activity

--- app/src/main/java/com/example/nutrifit/activities/AnhadirComidaActivity.kt
+++ app/src/main/java/com/example/nutrifit/activities/AnhadirComidaActivity.kt
@@ -1,6 +1,28 @@
 package com.example.nutrifit.activities
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.nutrifit.R
+import android.view.View
+import android.widget.TextView
 
 class AnhadirComidaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anhadircomida)
+
+        val back: TextView = findViewById(R.id.volverMain)
+
+        back.setOnClickListener {
+            finish()
+        }
+
+
+    }
+
+
+
+
 }
